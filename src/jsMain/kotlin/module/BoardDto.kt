a975810462f19bb24c3ff5c552f3c84597fa012f
lovehzj <1922919664@qq.com>
2022-04-05 21:51:43 +0800

    add focus param

--- src/jsMain/kotlin/module/BoardDto.kt
+++ src/jsMain/kotlin/module/BoardDto.kt
@@ -9,9 +9,9 @@ class BoardDto(size: Int) {
             arrayOf(
                 arrayOf(
                     arrayOf(
-                        DigitBoardDto(value = 1, initNum = false, error = false, focus = false),
-                        DigitBoardDto(value = 2, initNum = false, error = false, focus = false),
-                        DigitBoardDto(value = 3, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                     arrayOf(
                         DigitBoardDto(value = null, initNum = false, error = false, focus = false),

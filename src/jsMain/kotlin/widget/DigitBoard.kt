80ce4f894d3ba6269e84dc4462f10d1757bb5d5e
TingTing Wang <1922919664@qq.com>
2022-04-05 22:14:10 +0800

    add render of color (#5)

--- src/jsMain/kotlin/widget/DigitBoard.kt
+++ src/jsMain/kotlin/widget/DigitBoard.kt
@@ -2,13 +2,47 @@ package widget
 
 import androidx.compose.runtime.Composable
 import module.DigitBoardDto
+import org.jetbrains.compose.web.css.CSSColorValue
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.backgroundColor
+import org.jetbrains.compose.web.css.color
+import org.jetbrains.compose.web.dom.Span
 import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun DigitBoard(boardDto: DigitBoardDto) {
-    if (boardDto.value.value == null) {
-        Text("")
+    val bacCol: CSSColorValue
+    val textCol: CSSColorValue
+    val text: String = if (boardDto.value.value == null) {
+        ""
     } else {
-        Text(boardDto.value.value.toString())
+        boardDto.value.value.toString()
+    }
+    if (boardDto.focus) {
+        bacCol = Color.blue
+        textCol = if (boardDto.error) {
+            Color.red
+        } else {
+            Color.white
+        }
+    } else {
+        bacCol = Color.transparent
+        textCol = if (boardDto.error) {
+            Color.red
+        } else if (boardDto.initNum.value) {
+            Color.black
+        } else {
+            Color.blue
+        }
+    }
+    Span(
+        attrs = {
+            style {
+                color(textCol)
+                backgroundColor(bacCol)
+            }
+        } // inline style
+    ) {
+        Text(text)
     }
 }

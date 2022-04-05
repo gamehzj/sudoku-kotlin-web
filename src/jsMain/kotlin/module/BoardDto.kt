7f2679ce56d9fb5e68db86f084be0b2269939c42
lovehzj <1922919664@qq.com>
2022-04-05 21:39:02 +0800

    add focus param

--- src/jsMain/kotlin/module/BoardDto.kt
+++ src/jsMain/kotlin/module/BoardDto.kt
@@ -9,159 +9,159 @@ class BoardDto(size: Int) {
             arrayOf(
                 arrayOf(
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = true, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = true, error = true, focus = false),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = true, error = true, focus = true),
+                        DigitBoardDto(value = null, initNum = false, error = true, focus = true),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = true),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                ),
-                arrayOf(
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                 ),
                 arrayOf(
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                ),
-            ),
-            arrayOf(
-                arrayOf(
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                 ),
                 arrayOf(
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                ),
-                arrayOf(
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                 ),
             ),
             arrayOf(
                 arrayOf(
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                ),
-                arrayOf(
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                    ),
-                    arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                 ),
                 arrayOf(
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                     arrayOf(
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
-                        DigitBoardDto(value = null, initNum = false, error = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                ),
+                arrayOf(
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                ),
+            ),
+            arrayOf(
+                arrayOf(
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                ),
+                arrayOf(
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                ),
+                arrayOf(
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                    ),
+                    arrayOf(
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
+                        DigitBoardDto(value = null, initNum = false, error = false, focus = false),
                     ),
                 ),
             ),

5e16fe7c1b8b67901cebba8276f1768d3889fd7c
lovehzj <1922919664@qq.com>
2022-04-05 17:49:21 +0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.compose.compose
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.1"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+}
+
+group = "com.github.gamehzj"
+version = "0.0.1"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    js(IR) {
+        browser {
+            testTask {
+                testLogging.showStandardStreams = true
+                useKarma {
+                    useChromeHeadless()
+                    useFirefox()
+                }
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
+}
---
+++ src/jsMain/kotlin/Main.kt
@@ -0,0 +1,6 @@
+import org.jetbrains.compose.web.renderComposable
+
+fun main() {
+    renderComposable(rootElementId = "root") {
+    }
+}
